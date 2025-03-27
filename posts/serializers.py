acfe619ab5cb8b4e7d6fe534628561ed5b26ff4d
harrypmdev <harrypetermiles@gmail.com>
2025-03-27 21:04:27 +0000

    feat: add user upvoted/downvoted fields to post and song serialization

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Post
+from post_votes.models import PostVote
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,15 +9,30 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='user.profile.id')
     user_image = serializers.ReadOnlyField(source='user.profile.image.url')
     is_user = serializers.SerializerMethodField()
+    user_upvoted = serializers.SerializerMethodField()
+    user_downvoted = serializers.SerializerMethodField()
 
     def get_is_user(self, obj):
         request = self.context['request']
         return request.user == obj.user
+
+    def get_user_upvoted(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return PostVote.objects.filter(post=obj, user=user, downvote=False).exists()
+        return False
+
+    def get_user_downvoted(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return PostVote.objects.filter(post=obj, user=user, downvote=True).exists()
+        return False
 
     class Meta:
         model = Post
         fields = [
             'id', 'user', 'user_image', 'profile_id',
             'is_user', 'title', 'updated_at',
-            'created_at', 'content', 'song', 'net_votes'
+            'created_at', 'content', 'song', 'net_votes',
+            'user_upvoted', 'user_downvoted'
         ]
--- songs/serializers.py
+++ songs/serializers.py
@@ -1,20 +1,35 @@
 from rest_framework import serializers
 
 from .models import Song
+from song_votes.models import SongVote
 
 
 class SongSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     is_user = serializers.SerializerMethodField()
+    user_upvoted = serializers.SerializerMethodField()
+    user_downvoted = serializers.SerializerMethodField()
 
     def get_is_user(self, obj):
         request = self.context['request']
         return request.user == obj.user
+
+    def get_user_upvoted(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return SongVote.objects.filter(song=obj, user=user, downvote=False).exists()
+        return False
+
+    def get_user_downvoted(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return SongVote.objects.filter(song=obj, user=user, downvote=True).exists()
+        return False
 
     class Meta:
         model = Song
         fields = [
             'id', 'user', 'is_user', 'title', 'updated_at',
             'created_at', 'audio_url', 'net_votes', 'artist_name',
-            'link_to_song', 'audio_file'
+            'link_to_song', 'audio_file', 'user_upvoted', 'user_downvoted'
         ]
