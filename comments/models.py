246763341fdc0fcc6e0f9f02522242b7e5bfd728
harrypmdev <harrypetermiles@gmail.com>
2025-03-30 12:33:55 +0100

    feat: add updated_at to Comment model, update serializer

--- comments/models.py
+++ comments/models.py
@@ -8,6 +8,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     class Meta:
--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 
 from .models import Comment
@@ -5,15 +6,26 @@ from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    profile_id = serializers.ReadOnlyField(source='user.profile.id')
+    profile_image = serializers.ReadOnlyField(source='user.profile.image.url')
     is_user = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_user(self, obj):
         request = self.context['request']
         return request.user == obj.user
 
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
     class Meta:
         model = Comment
         fields = [
-            'id', 'user', 'is_user',
-            'created_at', 'content', 'post'
+            'id', 'user', 'profile_id', 'profile_image',
+            'is_user', 'created_at', 'updated_at',
+            'content', 'post'
         ]
