b43f001c7ba7de87d4dcf51191882ab4f31cf946
harrypmdev <harrypetermiles@gmail.com>
2025-04-04 09:49:16 +0100

    feat: alter UserVenueSerializer to better match VenueSerializer, add user_count

--- venues/serializers.py
+++ venues/serializers.py
@@ -14,11 +14,17 @@ class VenueSerializer(serializers.ModelSerializer):
 
 
 class UserVenueSerializer(serializers.ModelSerializer):
-    user_name = serializers.ReadOnlyField(source="user.username")
-    venue_name = serializers.ReadOnlyField(source="venue.name")
+    user_id = serializers.ReadOnlyField(source="user.id")
+    user = serializers.ReadOnlyField(source="user.username")
+    venue_id = serializers.ReadOnlyField(source="user.id")
+    name = serializers.ReadOnlyField(source="venue.name")
+    user_count = serializers.SerializerMethodField()
+
+    def get_user_count(self, obj):
+        return obj.venue.venue_users.count()
 
     class Meta:
         model = UserVenue
         fields = [
-            'id', 'user', 'user_name',
-            'venue', 'venue_name', 'created_at']
+            'id', 'user', 'user_id',
+            'venue_id', 'name', 'user_count', 'created_at']
